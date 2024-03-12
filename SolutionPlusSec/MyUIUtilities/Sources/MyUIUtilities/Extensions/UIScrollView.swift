9018d0e32717fe6c3c2b20c2ef8a0a9ee2bc0f32
MRamadann <muhammedramadan607@gmail.com>
2024-03-12 14:34:49 +0200

    third

--- SolutionPlusSec/MyUIUtilities/Sources/MyUIUtilities/Extensions/UIScrollView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Apple on 11/03/2024.
-//
-
-import Foundation
--- SolutionPlusSec/MyUIUtilities/Sources/MyUIUtilities/Extensions/UIView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File 2.swift
-//  
-//
-//  Created by Apple on 11/03/2024.
-//
-
-import Foundation
